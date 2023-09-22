ee721b35dce4d256a7e1fc07fcd72e2cc4ea30f4
cookie1986 <cookd.liv@gmail.com>
2023-09-22 10:45:15 +0100

    updated comments

--- UserSignup/__init__.py
+++ UserSignup/__init__.py
@@ -1,37 +1,39 @@
 import logging
 from dotenv import load_dotenv
+load_dotenv()
+
 import os
 
 import azure.functions as func
 
 # get api key from the receiver side (local only)
-load_dotenv()
 RECEIVER_API = os.environ.get('RECEIVER-API-KEY')
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('UserSignup HTTP trigger function processed an incoming request.')
 
     # authorisation
     SENDER_API = req.headers.get('SENDER-API-KEY')
-    logging.info(f'Access attempted with API Key: {SENDER_API}')
     if RECEIVER_API != SENDER_API:
+        logging.info(f'Access attempted with an incorrect API Key: {SENDER_API}')
         return func.HttpResponse("Invalid API Key", status_code=401)
 
-    # process the incoming request
-    name = req.params.get('name')
-    if not name:
+    # if API key is approved, process the incoming request
+    email = req.params.get('email')
+    if not email:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
-
-    if name:
-        print(name)
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+            email = req_body.get('email')
+    
+    # if email is in payload
+    if email:
+        logging.info(f'User with email {email} has completed signup')
+        return func.HttpResponse(f"Hello, {email}. This HTTP triggered function executed successfully.")
     else:
-        print("cant find the name")
+        logging.info(f'Incoming request did not include email in payload.')
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
